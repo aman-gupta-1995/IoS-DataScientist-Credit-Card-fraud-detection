42294cf0a546a933c616f659891a95aafda8920d
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-05 14:50:55 +0530

    Update AKMaskField.swift

--- IoS & DataScientist- Credot Card fraud detection [Adobe, Noida]/CreditCardForm/Classes/AKMaskField.swift
+++ IoS & DataScientist- Credot Card fraud detection [Adobe, Noida]/CreditCardForm/Classes/AKMaskField.swift
@@ -5,7 +5,7 @@
 //      
 //  Created by Artem Krachulov       
 //  Copyright (c) 2016 Artem Krachulov. All rights reserved.       
-//  Website: http://www.artemkrachulov.com/     
+//  Website: http://www.artemkrachulov.com/      
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 // and associated documentation files (the "Software"), to deal in the Software without restriction,
